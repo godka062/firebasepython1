18405db3ec13c95c7c740118fc7124972925f458
godka062 <104710977+godka062@users.noreply.github.com>
2024-05-04 13:16:22 +0900

    Update firebasepython.py

--- firebasepython.py
+++ firebasepython.py
@@ -2,10 +2,20 @@ import os
 import firebase_admin
 from firebase_admin import credentials, initialize_app
 
-# 환경 변수에서 자격 증명 파일 경로 가져오기
-cred_path = os.getenv('FIREBASE_CREDENTIALS')
-if cred_path and os.path.exists(cred_path):
-    cred = credentials.Certificate(cred_path)
-    app = initialize_app(cred)
-else:
-    raise ValueError("Firebase credentials path is not set or invalid")
+# Streamlit Secrets에서 자격증명 정보 가져오기
+cred_obj = {
+    "type": os.environ["firebase_type"],
+    "project_id": os.environ["firebase_project_id"],
+    "private_key_id": os.environ["firebase_private_key_id"],
+    "private_key": os.environ["firebase_private_key"].replace('\\n', '\n'),
+    "client_email": os.environ["firebase_client_email"],
+    "client_id": os.environ["firebase_client_id"],
+    "auth_uri": os.environ["firebase_auth_uri"],
+    "token_uri": os.environ["firebase_token_uri"],
+    "auth_provider_x509_cert_url": os.environ["firebase_auth_provider_x509_cert_url"],
+    "client_x509_cert_url": os.environ["firebase_client_x509_cert_url"]
+}
+
+# 파이어베이스 초기화
+cred = credentials.Certificate(cred_obj)
+app = initialize_app(cred)
